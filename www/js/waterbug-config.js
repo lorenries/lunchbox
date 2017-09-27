fc2f6acfca98f440f8313c39200c2be2bd9963f3
lorenries <lorenries@users.noreply.github.com>
2017-09-27 11:50:37 -0400

    add WOLA logo options to waterbug

--- www/js/waterbug-config.js
+++ www/js/waterbug-config.js
@@ -5,20 +5,14 @@ var elementPadding = 40; // padding around the logo and credit text
 // logo configuration
 // the name of the logo object should match the value of the corresponding radio button in the HTML.
 var logos = {
-    'lunchbox': {
-        whitePath: '../img/icon-lunchbox-white.svg', // path to white logo
-        blackPath: '../img/icon-lunchbox-black.svg', // path to black logo
-        w: 100, // width of logo
+    'wola': {
+        whitePath: '../img/WOLA_icon_white.png', // path to white logo
+        blackPath: '../img/WOLA_icon_grey.png', // path to black logo
+        w: 80, // width of logo
         h: 80, // height of logo
-        display: 'Lunchbox'
+        display: 'WOLA'
     },
-    'socializr': {
-        whitePath: '../img/icon-socializr-white.svg',
-        blackPath: '../img/icon-socializr-black.svg',
-        w: 150,
-        h: 51,
-        display: 'Socializr'
-    }
+    'none': {display: 'None'}
 };
 
 // logo opacity for colors
@@ -35,7 +29,7 @@ var fontShadowOffsetY = 0; // font shadow offset y
 var fontShadowBlur = 10; // font shadow blur
 
 // copyright options
-var orgName = 'Your News Organization';
+var orgName = 'WOLA';
 var freelanceString = 'for ' + orgName;
 
 var copyrightOptions = {
@@ -83,7 +77,7 @@ var copyrightOptions = {
 
 // app load defaults
 var currentCrop = 'twitter'; // default crop size
-var currentLogo = 'lunchbox'; // default logo slug
+var currentLogo = 'wola'; // default logo slug
 var currentLogoColor = 'white'; // default logo color
 var currentTextColor = 'white'; // default text color
 var defaultImage = '../img/test-kitten.jpg'; // path to image to load as test image
